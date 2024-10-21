3ac12a1f85d811ea28177164bcbb2e8ba1da918c
dev-shenlong <149168521+dev-shenlong@users.noreply.github.com>
2024-10-20 21:46:12 -0700

    Update bus_interface_unit.vhd

--- VHDL_MP_8085.srcs/sources_1/new/bus_interface_unit.vhd
+++ VHDL_MP_8085.srcs/sources_1/new/bus_interface_unit.vhd
@@ -1,14 +1,14 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Group: Dipanshu and Aryan 
+-- 
 -- 
 -- Create Date: 20.10.2024 20:58:57
 -- Design Name: 
 -- Module Name: bus_interface_unit - Behavioral
--- Project Name: 
--- Target Devices: 
+-- Project Name: 8085 MP
+-- Target Devices: XCS750 CSGA324ABX2321 
 -- Tool Versions: 
--- Description: 
+-- Description: This file describes the bus interface unit of the 8085 MP
 -- 
 -- Dependencies: 
 -- 
@@ -22,21 +22,27 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity bus_interface_unit is
---  Port ( );
+    Generic (
+        clk:std_logic   -- The clock input internally connected
+        
+    );
+    Port (
+        A: out std_logic_vector(7 downto 0); -- Address bus that carry the higher address bus
+        AD: inout std_logic_vector(7 downto 0); -- Address and Data bus multiplexed with lower Adrress bus
+        S: out std_logic_vector(1 downto 0);
+        io_not_m : out std_logic;
+        start: in std_logic;
+        done: out std_logic; -- Status 
+        command: in std_logic_vector(7 downto 0) -- DoData Pulse to interpret which command to carry out
+   );
+   
 end bus_interface_unit;
 
 architecture Behavioral of bus_interface_unit is
-
+    component bus_interface_instruction_decoder is
+    end component;
+    
 begin
 
 
