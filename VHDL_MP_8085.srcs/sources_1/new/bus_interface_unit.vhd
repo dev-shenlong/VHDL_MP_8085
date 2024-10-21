79f1b9b2207527500a7e862cb89d9d32bccc2c4a
dev-shenlong <149168521+dev-shenlong@users.noreply.github.com>
2024-10-20 21:21:47 -0700

    Added project guidelines

---
+++ VHDL_MP_8085.srcs/sources_1/new/bus_interface_unit.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 20.10.2024 20:58:57
+-- Design Name: 
+-- Module Name: bus_interface_unit - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity bus_interface_unit is
+--  Port ( );
+end bus_interface_unit;
+
+architecture Behavioral of bus_interface_unit is
+
+begin
+
+
+end Behavioral;
